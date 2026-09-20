0d2a6ccaec33f0fb18eb7a62ac098e4460654f8f
Alexa Garcia <brisa.garcia3836@alumnos.udg.mx>
2026-09-20 00:25:42 -0600

    Update Programacion_estructurada-cajeroautomatico.py

--- Programacion_estructurada-cajeroautomatico.py
+++ Programacion_estructurada-cajeroautomatico.py
@@ -1,1 +1,23 @@
+multiplo_valido = 50
+limite_diario = 6000.0
 
+def retiro_cajero():
+    saldo = float(input("Saldo Actual: "))
+    retirado_hoy = float(input("Monto acumulado hoy: "))
+    monto = float(input("Monto a Retirar: "))
+
+    if monto % multiplo_valido != 0:
+        mensaje = "Monto no valido"
+    elif monto > saldo:
+        mensaje = "Saldo Insuficiente"
+    elif (retirado_hoy + monto) > limite_diario:
+        mensaje = "Limite Diario Excedido"
+    else:
+        saldo -= monto
+        mensaje = "Entregado"
+
+    
+    print(f"Resultado: {mensaje} | saldo final: ${saldo:.2f}")
+
+
+    retiro_cajero()
